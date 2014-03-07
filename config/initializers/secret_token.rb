981de6153accba632ee54f4a695d5b6faf66d283
Rolf Pedersen <rolfhsp@gmail.com>
2014-03-07 23:47:53 +0100

    First commit. Vanilla Rails 4.0.3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JqueryRails::Application.config.secret_key_base = 'b9c0d0621fc0613f5a05f9392dfef5418b81afee2e778f6f7e0fff2c006efb6763bb9de6ce7403490eda8d1611e02822ffbc95cfd5b03e364cb3e4c8e652ff98'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JqueryRails::Application.config.session_store :cookie_store, key: '_jquery-rails_session'
